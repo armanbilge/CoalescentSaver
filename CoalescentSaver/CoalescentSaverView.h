3b502670143b3ed820031e9c869121651efd7c34
Arman Bilge <armanbilge@gmail.com>
2014-06-10 01:47:14 +1200

    Initial Commit

---
+++ CoalescentSaver/CoalescentSaverView.h
@@ -0,0 +1,13 @@
+//
+//  CoalescentSaverView.h
+//  CoalescentSaver
+//
+//  Created by Arman Bilge on 10/06/14.
+//  Copyright (c) 2014 Computational Evolution Group. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface CoalescentSaverView : ScreenSaverView
+
+@end
---
+++ CoalescentSaver/CoalescentSaverView.m
@@ -0,0 +1,52 @@
+//
+//  CoalescentSaverView.m
+//  CoalescentSaver
+//
+//  Created by Arman Bilge on 10/06/14.
+//  Copyright (c) 2014 Computational Evolution Group. All rights reserved.
+//
+
+#import "CoalescentSaverView.h"
+
+@implementation CoalescentSaverView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
