97155f1329c55105853bcfbf94689780a1b2e868
Arman Bilge <armanbilge@gmail.com>
2014-06-10 14:41:26 +1200

    Update file copyright info

--- CoalescentSaver/Utils.m
+++ CoalescentSaver/Utils.m
@@ -1,9 +1,24 @@
 //
 //  Utils.m
-//  coalsave
 //
-//  Created by Arman Bilge on 9/06/14.
-//  Copyright (c) 2014 Computational Evolution Group. All rights reserved.
+//  CoalescentSaver
+//  A Mac screensaver derived from Trevor Bedford's coaltrace
+//      <https://github.com/trvrb/coaltrace>
+//
+//  Copyright (C) 2014 Arman D. Bilge <armanbilge@gmail.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #import <ScreenSaver/ScreenSaver.h>
